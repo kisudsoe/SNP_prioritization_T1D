c9defa05ad67dc7b05067e8fe298a0649303b23f
SeungSoo Kim <kisudsoe@gmail.com>
2019-01-10 18:15:23 -0500

    delete: temporary files

--- src/fname.r
+++
@@ -1,7 +0,0 @@
-library('tools')
-fname = function(path) {
-	#name = sub(pattern = "(.*)\\..*$", replacement = "\\1", basename(path))
-	name = file_path_sans_ext(path)
-	ext  = file_ext(path)
-	return(c(name,ext))
-}
