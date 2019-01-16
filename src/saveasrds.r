968ea1e814d526320fae1abda0b48bdff35fc5d8
SeungSoo Kim <kisudsoe@gmail.com>
2019-01-16 18:57:05 -0500

    write: change db data into rds files

--- src/saveasrds.r
+++ src/saveasrds.r
@@ -1,7 +1,17 @@
 # Read file and save as rds file
 library(data.table)
+library(tools)
 saveasrds = function(f_path) {
     df = fread(f_path,sep='\t',header=T,stringsAsFactors=F)
     f_name = paste0(f_path,'.rds')
-    save(df,file=f_name)
+    saveRDS(df,file=f_name)
+    cat(paste0('>> File write: ',f_name,'\n'))
+}
+
+library(rtracklayer)
+bedasrds = function(f_path) {
+    df = import(f_path,format='bed')
+    f_name = paste0(f_path,'.rds')
+    saveRDS(df,file=f_name)
+    cat(paste0('>> File write: ',f_name,'\n'))
 }
