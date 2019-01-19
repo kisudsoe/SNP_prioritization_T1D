01f937960de4976195c7989729de0163b7968cc8
SeungSoo Kim <kisudsoe@gmail.com>
2019-01-18 20:09:35 -0500

    update: read RDS file

--- roadmap_dn.r
+++ roadmap_dn.r
@@ -1,5 +1,5 @@
 #!/usr/bin/env Rscript
-# This file is for downloading RoadMap data
+# This file is for downloading RoadMap data.
 
 ## Command Arg Parameters ##
 # roadmap.bat: Rscript roadmap_dn.r
@@ -9,6 +9,7 @@ if(length(args)>0) stop(hmsg)
 
 # System parameter
 source('src/pdtime.r')
+source('src/saveasrds.r')
 t0 = Sys.time()
 
 # mkdir db/roadmap
@@ -26,11 +27,16 @@ cid = as.character(formatC(c(1:129),width=3,flag='0'))
 urls = paste0('https://egg2.wustl.edu/roadmap/data/byFileType/chromhmmSegmentations/ChmmModels/imputed12marks/jointModel/final/E',cid,'_25_imputed12marks_hg38lift_dense.bed.gz')
 #head(urls)
 t=lapply(urls,function(url) {
-	f.name = paste0(dir,basename(url))
-	tb = try(download.file(url,destfile=f.name)) # debug no file error
+	f_name = paste0(dir,basename(url))
+	tb = try(download.file(url,destfile=f_name)) # debug no file error
 	if("try-error" %in% class(tb)) tb=NULL
+    cat(paste0('\n>> ',f_name,'\n'))
+    try(R.utils::gunzip(f_name))
+    f_name2 = tools::file_path_sans_ext(f_name)
+    cat(paste0('>> ',f_name2,'\n'))
+    try(bedasrds(f_name2))
 })
-cat(paste0('>> ',pdtime(t0,1),'\n'))
+cat(paste0('\n>> ',pdtime(t0,1),'\n'))
 ##################
 ## Function end ##
 ##################
--- roadmap_filt.r
+++ roadmap_filt.r
@@ -2,14 +2,14 @@
 # This file is for filtering RoadMap data
 
 ## Command Arg Parameters ##
-# roadmap.bat: Rscript roadmap_filt.r
+# CMD usage: Rscript roadmap_filt.r
 args = commandArgs(trailingOnly=T)
 hmsg = 'Rscript roadmap_filt.r is needed no argument.'
 if(length(args)>0) stop(hmsg)
 dir = 'db/roadmap/'
 
 # System parameter
-library(rtracklayer)
+#library(rtracklayer)
 library(plyr)
 source('src/pdtime.r')
 t0 = Sys.time()
@@ -17,14 +17,16 @@ t0 = Sys.time()
 #########################
 ## Function start here ##
 #########################
-# 1. Load BED files
+# 1. Load RDS files
+cat("\n(1/2) Load RDS files\n")
 cid = as.character(formatC(c(1:129),width=3,flag='0'))
-path = paste0(dir,'E',cid,'_25_imputed12marks_hg38lift_dense.bed.gz')
+path = paste0(dir,'E',cid,'_25_imputed12marks_hg38lift_dense.bed.rds')
 road.li=list(); n=length(path)
 pb = winProgressBar(title='Loading files',
-	 label='Ready to read BED files..',min=0,max=n,width=500)
+	 label='Ready to read RDS files..',min=0,max=n,width=500)
 for(i in 1:n) {
-	tb1 = try(import(path[i],format='bed')) # rtracklayer
+	#tb1 = try(import(path[i],format='bed')) # rtracklayer
+    tb1 = try(readRDS(path[i]))
 	if('try-error' %in% class(tb1)) road.li[[i]] = NULL # get error from no file
 	else road.li[[i]] = tb1 # No errors
 	## Progress time ##
@@ -33,11 +35,11 @@ for(i in 1:n) {
     ###################
 }
 close(pb)
-cat(paste0('\n>> BED file read, ',pdtime(t0,2),'\n'))
-road.df = ldply(road.li, data.frame)
+cat(paste0('\n>> RDS file read, ',pdtime(t0,2),'\n'))
+road.df = ldply(road.li, data.frame) #plyr
 
 # 2. Filtering by enhancers and save as BED file
-cat(">> Filtering RoadMap data by enhancers\n")
+cat("\n(2/2) Filtering RoadMap data by enhancers\n")
 road.enh= subset(road.df,name %in% c("13_EnhA1","14_EnhA2","15_EnhAF","16_EnhW1","17_EnhW2","18_EnhAc"))
 f.name = 'db/roadmap_enh.bed'
 write.table(road.enh[,c(1:3,6)],f.name,row.names=F,col.names=F,quote=F,sep='\t')
