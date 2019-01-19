fff51aa8ea273928280f8829cecce469995ccc21
SeungSoo Kim <kisudsoe@gmail.com>
2019-01-18 20:08:48 -0500

    ipynb update

--- .ipynb_checkpoints/roadmap_filt-checkpoint.r
+++ .ipynb_checkpoints/roadmap_filt-checkpoint.r
@@ -2,7 +2,7 @@
 # This file is for filtering RoadMap data
 
 ## Command Arg Parameters ##
-# roadmap.bat: Rscript roadmap_filt.r
+# CMD usage: Rscript roadmap_filt.r
 args = commandArgs(trailingOnly=T)
 hmsg = 'Rscript roadmap_filt.r is needed no argument.'
 if(length(args)>0) stop(hmsg)
@@ -17,12 +17,13 @@ t0 = Sys.time()
 #########################
 ## Function start here ##
 #########################
-# 1. Load BED files
+# 1. Load RDS files
+cat("\n(1/2) Load RDS files\n")
 cid = as.character(formatC(c(1:129),width=3,flag='0'))
 path = paste0(dir,'E',cid,'_25_imputed12marks_hg38lift_dense.bed.rds')
 road.li=list(); n=length(path)
 pb = winProgressBar(title='Loading files',
-	 label='Ready to read BED files..',min=0,max=n,width=500)
+	 label='Ready to read RDS files..',min=0,max=n,width=500)
 for(i in 1:n) {
 	#tb1 = try(import(path[i],format='bed')) # rtracklayer
     tb1 = try(readRDS(path[i]))
@@ -34,11 +35,11 @@ for(i in 1:n) {
     ###################
 }
 close(pb)
-cat(paste0('\n>> BED file read, ',pdtime(t0,2),'\n'))
+cat(paste0('\n>> RDS file read, ',pdtime(t0,2),'\n'))
 road.df = ldply(road.li, data.frame) #plyr
 
 # 2. Filtering by enhancers and save as BED file
-cat(">> Filtering RoadMap data by enhancers\n")
+cat("\n(2/2) Filtering RoadMap data by enhancers\n")
 road.enh= subset(road.df,name %in% c("13_EnhA1","14_EnhA2","15_EnhAF","16_EnhW1","17_EnhW2","18_EnhAc"))
 f.name = 'db/roadmap_enh.bed'
 write.table(road.enh[,c(1:3,6)],f.name,row.names=F,col.names=F,quote=F,sep='\t')
