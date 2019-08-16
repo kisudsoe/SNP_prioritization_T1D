b127ef1acc28b432b7dcef17820aead8584f1ea1
SeungSoo Kim <kisudsoe@gmail.com>
2019-08-16 17:58:58 -0400

    Update venn.r - Restrict file numbers to make venn diagram

--- venn.r
+++ venn.r
@@ -7,9 +7,10 @@
 args = commandArgs(trailingOnly=T)
 hmsg = 'Rscript regulome.r [SNP_BED_file_1] [SNP_BED_file_2] [SNP_BED_file_3] or [SNP_BED_file_4]
   - Arguments [SNP_BED_file_1], [SNP_BED_file_2], and [SNP_BED_file_3] are needed.
-  - Argument [SNP_BED_file_4] is optional.'
+  - Argument [SNP_BED_file_4] is optional.
+    - If argument number is over 4, then venn diagram is not generated.'
 n = length(args)
-if(n < 1) stop(hmsg)
+if(n < 2) stop(hmsg)
 f.path = NULL
 for(i in 1:n) {
 	f.path = c(f.path,unlist(args[i]))
@@ -51,14 +52,14 @@ union = as.data.frame(union)	# Make 'union' to data.frame from
 colnames(union) = subtitle			# Names attach to venn diagram
 
 # Draw Venn Diagram
-if(n>3) {
+if(n>3 & n<5) {
 	f.name1 = paste0('fig/venn_',names[1],'_',names[2],'.png')
 	title1 = paste0('Venn analysis of ',nrow(union),' SNPs')
 	png(f.name1,width=10,height=10,units='in',res=100)
 	vennDiagram(union, main=title1, circle.col=rainbow(length(subtitle))) # limma
 	dev.off()
 	cat(paste0('\nFigure draw: ',f.name1,'\n'))
-}
+} else if(n>4) cat("Message: Can't plot Venn diagram for more than 5 sets.\n")
 
 # Write files
 colnames(union) = names
@@ -72,14 +73,14 @@ cat(paste0('File write: ',f.name2,'\n'))
 f.name3 = paste0('data/vennCounts.tsv')
 write.table(venn.li[[2]],f.name3,row.names=F,col.names=T,quote=F,sep='\t')
 cat(paste0('File write: ',f.name3,'\n'))
-f.name3a= paste0('fig/euler_',names[1],'_',names[2],'.png')
 
 # Draw Euler plot
 if(n==3) {
+	f.name3a = paste0('fig/euler_',names[1],'_',names[2],'.png')
 	png(f.name3a,width=10,height=10,units='in',res=100)
 	venn_c = unlist(venn.li[[2]][,4])
-	venn_fit = euler(c(
-		'A'    =as.numeric(venn_c[5]), # How to automate this?!
+	venn_fit = euler(c( # How to automate this?!
+		'A'    =as.numeric(venn_c[5]), 
 		'B'    =as.numeric(venn_c[3]),
 		'C'    =as.numeric(venn_c[2]),
 		'A&B'  =as.numeric(venn_c[7]),
@@ -98,14 +99,15 @@ if(n==3) {
 	print(p)
 	dev.off()
 	cat(paste0('\nFigure draw: ',f.name3a,'\n'))
+	
+	# Write core snp list from the three groups as snp_#_core.bed file.
+	f.name4 = paste0('data/snp_',nrow(core.df),'_core.bed')
+	core.df = union.out[(union.out[,5]==TRUE&
+						 union.out[,6]==TRUE&
+						 union.out[,7]==TRUE),c(2:4,1)]
+	write.table(core.df,f.name4,row.names=F,col.names=F,quote=F,sep='\t')
+	cat(paste0('File write: ',f.name4,'\n'))
 }
-
-core.df = union.out[(union.out[,5]==TRUE&
-					 union.out[,6]==TRUE&
-					 union.out[,7]==TRUE),c(2:4,1)]
-f.name4 = paste0('data/snp_',nrow(core.df),'_core.bed')
-write.table(core.df,f.name4,row.names=F,col.names=F,quote=F,sep='\t')
-cat(paste0('File write: ',f.name4,'\n'))
 cat(paste0('\n>> ',pdtime(t0,1),'\n'))
 ##################
 ## Function end ##
